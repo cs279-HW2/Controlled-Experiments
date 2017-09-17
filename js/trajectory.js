b874318788e52036a7daf09523323250eb6e35d8
SophieSi <xu_si@g.harvard.edu>
2017-09-17 16:37:38 -0400

    New changes

--- js/trajectory.js
+++ js/trajectory.js
@@ -1,3 +1,11 @@
 /**
  * Created by sophiasi on 9/13/17.
  */
+
+document.onmousemove = handleMouseMove;
+
+function handleMouseMove(event) {
+    doc = document.documentElement;
+    body = document.body;
+    console.log(event.clientX + " " + event.clientY);
+}
