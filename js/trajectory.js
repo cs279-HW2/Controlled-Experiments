afa7226eb14463e2a4887ac57ae74121967ae2db
SophieSi <xu_si@g.harvard.edu>
2017-09-17 23:01:41 -0400

    Add data storage

--- js/trajectory.js
+++ js/trajectory.js
@@ -21,15 +21,23 @@ function nextImg() {
 
 $.getJSON("./json/img_json.json", function(json) {
     img_json = json;
-    console.log(img_json);
+    // console.log(img_json);
     // for (i = 0; i < 66; i++) {
     //     console.log(json[i])
     // }
-    console.log(img_json[0])
+    // console.log(img_json[0])
     document.getElementById("box").style.backgroundImage = "url(" + img_json[0] + ")";
     document.getElementById("box").style.backgroundSize = "1350px";
     currentSession = session1;
 });
+
+var user = {name: "mark", age: "21", traject: [{x: 11, y:22}, {x: 22, y:33}]};
+
+localStorage.setItem("lastname", "Smith");
+localStorage.setItem('user', JSON.stringify(user));
+
+// console.log(localStorage.getItem("lastname"));
+
 
 
 
