d503f8a57122f81e5bb0425d52e0047af4da09c3
yunajoung <joung@college.harvard.edu>
2017-09-18 12:16:43 -0400

    cleaning and semicolons

--- js/onclick.js
+++ js/onclick.js
@@ -10,19 +10,7 @@ var buttonfemale = document.getElementById('buttonfemale');
 var buttonmale = document.getElementById('buttonmale');
 var buttonnext = document.getElementById('buttonnext');
 var index = 1000;
-// array to store images
-var images_list = [];
 
-/*
-// input images array
-for (var i = 0; i < images.length; i++) {
-	images_list[i] = images[i];
-	var currentImage = images_list[i];
-	// setup initial zIndex
-	// positive number is closer
-	currentImage.style.zIndex = -i;
-}
-*/
 function handleMouseMove(event) {
     doc = document.documentElement;
     body = document.body;
@@ -38,12 +26,6 @@ var count = 0;
 //click on next button, change pic and start tracing
 buttonnext.onclick = function(){
     $(buttonnext).attr("disabled", "disabled");
-	/*
-	var showFirst = images_list.shift();
-	// setup new zIndex
-	showFirst.style.zIndex = - index + count;
-	images_list.push(showFirst);
-	*/
 	console.log(count);
 	document.getElementById("box").style.backgroundImage = "url(" + img_json[count] + ")";
 	console.log(img_json[count]);
@@ -57,11 +39,11 @@ buttonnext.onclick = function(){
 
 //click on female or male button, stop tracing
 buttonfemale.onclick = function(){
-    $(buttonnext).removeAttr("disabled")
+    $(buttonnext).removeAttr("disabled");
 	document.onmousemove = handleMouseMoveEnd;
 }
 
 buttonmale.onclick = function(){
-    $(buttonnext).removeAttr("disabled")
+    $(buttonnext).removeAttr("disabled");
 	document.onmousemove = handleMouseMoveEnd;
 }
